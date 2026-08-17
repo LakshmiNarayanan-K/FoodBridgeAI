5f1d331ea385f5e320e2812eab0d5199382498cd
LakshmiNarayanan-K <lakshminarayanank.cse2025@citchennai.net>
2026-08-17 12:43:11 +0530

    Add donation lookup and delivery assignment routes

--- backend/routes/food_routes.py
+++ backend/routes/food_routes.py
@@ -4,27 +4,20 @@ from controllers.food_controller import (
     donate_food,
     get_all_food,
     get_my_food,
+    get_food,
     delete_food,
     update_status,
-    add_review
+    assign_delivery,
+    add_review,
 )
 
 food_bp = Blueprint("food", __name__)
 
-# Donate Food
 food_bp.route("/donate", methods=["POST"])(donate_food)
-
-# Get All Donations
 food_bp.route("/all", methods=["GET"])(get_all_food)
-
-# Get My Donations
 food_bp.route("/my", methods=["GET"])(get_my_food)
-
-# Delete Donation
+food_bp.route("/<int:food_id>", methods=["GET"])(get_food)
 food_bp.route("/delete/<int:food_id>", methods=["DELETE"])(delete_food)
-
-# Update Status
 food_bp.route("/status/<int:food_id>", methods=["PUT"])(update_status)
-
-# Add Receiver Review
+food_bp.route("/assign/<int:food_id>", methods=["PUT"])(assign_delivery)
 food_bp.route("/review/<int:food_id>", methods=["PUT"])(add_review)
